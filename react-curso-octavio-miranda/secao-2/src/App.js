463d90752acab26dd08c94de80fcf75795f73fd0
Diego Brito <diegorbrito9@gmail.com>
2021-12-18 15:40:10 -0300

    [react | seçao 2] lifecycle methods parte 1

--- react-curso-octavio-miranda/secao-2/src/App.js
+++ react-curso-octavio-miranda/secao-2/src/App.js
@@ -1,28 +1,50 @@
 import React from "react";
 
 class App extends React.Component {
-    
   state = {
     posts: [
-      {id: 1, title: 'titulo 1', body: 'conteudo 1'},
-      {id: 2, title: 'titulo 2', body: 'conteudo 2'},
-      {id: 3, title: 'titulo 3', body: 'conteudo 3'},
-      {id: 4, title: 'titulo 4', body: 'conteudo 4'},
+      { id: 1, title: "titulo 1", body: "conteudo 1" },
+      { id: 2, title: "titulo 2", body: "conteudo 2" },
+      { id: 3, title: "titulo 3", body: "conteudo 3" },
+      { id: 4, title: "titulo 4", body: "conteudo 4" },
     ],
+    counter: 0,
+  };
+  timeoutStop = null;
+
+  componentDidMount() {
+    this.handleTimeOut();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeoutStop);
+  }
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+
+    posts[0].title = "Novo titulo teste";
+
+    this.timeoutStop = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
+
     return (
       <div>
-        {
-          posts.map(post => (
-            <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
-            </div>
-          )) 
-        }
+        <p>{counter}</p>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
